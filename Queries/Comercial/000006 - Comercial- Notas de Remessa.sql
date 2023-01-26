bee1fc9eb587d2480a57e5af0131a8a9c48467e1
Roseclei <cleiventuras@gmail.com>
2023-01-26 15:21:11 -0300

    Adcionado cliente Entrega e repeticao da SA1

--- Queries/Comercial/000006 - Comercial- Notas de Remessa.sql
+++ Queries/Comercial/000006 - Comercial- Notas de Remessa.sql
@@ -1,8 +1,8 @@
 Select 
 	
      Item.R_E_C_N_O_ as Recno,  Rtrim(D2_LOCAL) + ' - ' + RTRIM(Armazem.NNR_DESCRI) as Armazem, 
-	 RTRIM(B6_CLIFOR) + ' - ' + Rtrim(B6_LOJA) as Cliente_ID,  Rtrim(A1_NOME) as Cliente, A1_NREDUZ as Fantasia,
-	 RTRIM(A1_MUN) as Cidade, A1_EST as UF,	 
+	 RTRIM(B6_CLIFOR) + ' - ' + Rtrim(B6_LOJA) as Cliente_ID,  Rtrim(Cliente.A1_NOME) as Cliente,  Cliente.A1_NREDUZ as Fantasia, F2_CLIENTE as CliEntregaID, A1_NOME as CliEntrega ,
+	 RTRIM(Cliente.A1_MUN) as Cidade, Cliente.A1_EST as UF,	 
 	 D2_DOC as Nota, F2_SERIE as Serie, D2_TES as TES_ID, F4_TEXTO as TES, D2_TIPO as Tipo, Year(D2_EMISSAO) as Ano,
 	 Convert(Varchar(10),Convert(Datetime,D2_EMISSAO,112),103) as Emissao, 
 	 
@@ -29,7 +29,7 @@ Select
 	 Case when B6_ATEND = 'S' then 'FINALIZADO' else 'EM ABERTO' end as Status,
 	 F2_PACIENT as Paciente, Case when F2_FSCCONV = '' then '' else F2_FSCCONV + ' - ' + F2_CONVENI end as Convenio, 
 	 Case when F2_DTPROCE <> '' then Convert(Varchar(10),Convert(Datetime,F2_DTPROCE,112),103) else '' end as DtProcedimento, 
-	 F2_FSNOME AS Usuario, Rtrim(A1_FSLOCAL) + ' - ' + RTRIM(ArmazemCliente.NNR_DESCRI) as ArmazemCliente,
+	 F2_FSNOME AS Usuario, Rtrim(Cliente.A1_FSLOCAL) + ' - ' + RTRIM(ArmazemCliente.NNR_DESCRI) as ArmazemCliente,
 	 
 	 (	 
 		Select 
@@ -67,17 +67,19 @@ Inner Join SB6010 PoderTerceiros (nolock) ON B6_FILIAL = '010101'
 										 AND B6_PRODUTO = D2_COD 
 										 AND B6_IDENT = D2_IDENTB6
                                          AND B6_PODER3 = 'R'
-Inner Join SA1010 (nolock) Cliente ON A1_FILIAL = ''
+Inner Join SA1010 (nolock) Cliente ON Cliente.A1_FILIAL = ''
 							      AND Cliente.D_E_L_E_T_ <> '*' 
-								  AND A1_COD = B6_CLIFOR 
-								  AND A1_LOJA = B6_LOJA	
+								  AND Cliente.A1_COD = B6_CLIFOR 
+								  AND Cliente.A1_LOJA = B6_LOJA	
+
 Left Join NNR010 ArmazemCliente (nolock)  ON ArmazemCliente.NNR_FILIAL = '010101'
 								  AND ArmazemCliente.D_E_L_E_T_ <> '*'	
-								  AND ArmazemCliente.NNR_CODIGO = A1_FSLOCAL								  
+								  AND ArmazemCliente.NNR_CODIGO = Cliente.A1_FSLOCAL								  
 
 Where F2_FILIAL = '010101'
 AND Nota.D_E_L_E_T_ <> '*' 
 AND F2_TIPO = 'N'
+AND F2_DOC = @DOC
 AND CONVERT(DATE,F2_EMISSAO,112) between :DATAINICIAL AND :DATAFIM
 
 UNION ALL
@@ -85,8 +87,8 @@ UNION ALL
 Select 
 	
      Item.R_E_C_N_O_ as Recno,  Rtrim(D2_LOCAL) + ' - ' + RTRIM(Armazem.NNR_DESCRI) as Armazem, 
-	 RTRIM(F2_CLIENTE) + ' - ' + Rtrim(F2_LOJA) as Cliente_ID,  Rtrim(A1_NOME) as Cliente, A1_NREDUZ as Fantasia,
-	 RTRIM(A1_MUN) as Cidade, A1_EST as UF,	 
+	 RTRIM(F2_CLIENTE) + ' - ' + Rtrim(F2_LOJA) as Cliente_ID,  Rtrim(Cliente.A1_NOME) as Cliente, Cliente.A1_NREDUZ as Fantasia,F2_CLIENT as CliEntregaID, Cli.A1_NOME as CliEntrega ,
+	 RTRIM(Cliente.A1_MUN) as Cidade, Cliente.A1_EST as UF,	 
 	 D2_DOC as Nota, F2_SERIE as Serie, D2_TES as TES_ID, F4_TEXTO as TES, D2_TIPO as Tipo, Year(D2_EMISSAO) as Ano,
 	 Convert(Varchar(10),Convert(Datetime,D2_EMISSAO,112),103) as Emissao, 
 	 
@@ -112,7 +114,7 @@ Select
 	 'FINALIZADO' as Status,
 	 F2_PACIENT as Paciente, Case when F2_FSCCONV = '' then '' else F2_FSCCONV + ' - ' + F2_CONVENI end as Convenio, 
 	 Case when F2_DTPROCE <> '' then Convert(Varchar(10),Convert(Datetime,F2_DTPROCE,112),103) else '' end as DataProcedimento, 
-	 F2_FSNOME AS Usuario, Rtrim(A1_FSLOCAL) + ' - ' + RTRIM(ArmazemCliente.NNR_DESCRI) as ArmazemCliente,
+	 F2_FSNOME AS Usuario, Rtrim(Cliente.A1_FSLOCAL) + ' - ' + RTRIM(ArmazemCliente.NNR_DESCRI) as ArmazemCliente,
 	 
 	 (	 
 		Select 
@@ -131,7 +133,7 @@ INNER JOIN SA1010 Cliente (nolock) ON A1_FILIAL = ''
 							      AND A1_LOJA = F2_LOJA	
 Left Join NNR010 ArmazemCliente (nolock)  ON ArmazemCliente.NNR_FILIAL = '010101'
 								  AND ArmazemCliente.D_E_L_E_T_ <> '*'	
-								  AND ArmazemCliente.NNR_CODIGO = A1_FSLOCAL								      
+								  AND ArmazemCliente.NNR_CODIGO = Cliente.A1_FSLOCAL								      
 INNER JOIN SD2010 Item (nolock) ON D2_FILIAL = '010101'
 							   AND Item.D_E_L_E_T_ <> '*' 
 							   AND D2_DOC = F2_DOC
@@ -139,6 +141,10 @@ INNER JOIN SD2010 Item (nolock) ON D2_FILIAL = '010101'
 							   AND D2_TIPO = F2_TIPO
 							   AND D2_CLIENTE = F2_CLIENTE
 							   AND D2_LOJA = F2_LOJA
+Inner Join SA1010 (nolock) Cli ON Cliente.A1_FILIAL = ''
+							      AND Cli.D_E_L_E_T_ <> '*' 
+								  AND Cli.A1_COD = Nota.F2_CLIENT
+								  AND Cli.A1_LOJA = F2_LOJENT
 Inner Join SB1010 (nolock) Produto ON B1_FILIAL = ''
 								  AND Produto.D_E_L_E_T_ <> '*'
 								  AND Produto.B1_COD = D2_COD
@@ -161,6 +167,7 @@ Inner Join SX5010 CFOP ON X5_FILIAL = ''
 Where F2_FILIAL = '010101'
 AND Nota.D_E_L_E_T_ <> '*' 
 AND F2_TIPO = 'N'
+AND F2_DOC = @DOC
 AND CONVERT(DATE,F2_EMISSAO,112) between :DATAINICIAL AND :DATAFIM
 
 Order by Emissao, Nota, Item
