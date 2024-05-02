5db24d6db631470e8c11c9ac167113d33d54ecf5
Andre Brito <andrebrito.analista@gmail.com>
2024-05-02 11:59:44 -0300

    Inclusão do ProtheusDoc nas rotinas SF2460I e SF2520E

--- html-out/data.js
+++ html-out/data.js
@@ -1,3 +1,3 @@
 
-		let dataProject = {"projectName":"","tree":{"name":"raiz","files":[{"file":"HelloWorld.prw","uniqueName":"HelloWorld.prw"}],"paths":[]},"files":[{"fileName":"HelloWorld.prw","functionList":[]}]}
+		let dataProject = {"projectName":"","tree":{"name":"raiz","files":[{"file":"SF2460I.prw","uniqueName":"SF2460I.prw"}],"paths":[]},"files":[{"fileName":"SF2460I.prw","functionList":[]}]}
 		
